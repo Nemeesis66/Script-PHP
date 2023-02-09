81981ef89f5941fee24619f23b3c72a80cc5fe9d
nemeesis66 <nemeesis.66@gmail.com>
2023-02-09 15:25:27 +0700

    Upload Script

---
+++ script1/config.php
@@ -0,0 +1,6 @@
+<?php 
+
+$deviceId = "549652f8c6fb0262";
+$userAgent = "Mozilla/5.0 (Linux; Android 11; RMX3201) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36";
+
+$simasterScript = "ON"; //Fill ON or OFF
